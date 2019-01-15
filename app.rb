675d88e6072f94f32c6a954007a175dbe5f92140
Brad <bradsaid@gmail.com>
2019-01-15 13:23:10 -0600

    Add words to the array

--- app.rb
+++ app.rb
@@ -3,21 +3,38 @@ require 'sinatra'
 get '/' do
     erb :index
   end
-  
-get '/about' do
-    erb :about
-end
-  
-get '/palindrome' do
-    erb :palindrome
+   
+get '/array' do
+    erb :arraymaker
 end
 
 get '/hangman' do
     erb :hangman
 end
 
+$testArray = []
+$randomWord
+$wordLength
+$wordDashes = []
 
-post '/check' do
-  @phrase = params[:phrase]
-  erb :result
+post '/submit' do
+  @phrase = params[:phrase]  # get value from form field
+  $testArray.push(@phrase)   # push into array
+  erb :arraymaker
 end
+
+post '/pick' do
+  $randomWord = $testArray.sample  # get random index from array 
+  $wordLength = $randomWord.length  # get length of word
+  $wordLength.times do      # loop through word and give _ per letter
+    $wordDashes.push("_")
+  end
+  erb :arraymaker
+end
+
+post '/clear' do
+  $testArray = []  # empty array
+  erb :arraymaker
+end
+
+
